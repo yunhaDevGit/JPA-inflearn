248557a073489ebe768f2660040cfb7242d90b0b
yunhaDev <yunhaDev98@gmail.com>
2021-05-13 18:26:25 +0900

    feat: test jpa

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -19,13 +19,13 @@ public class JpaMain {
         // 엔티티 매니저에서 수행하는 모든 로직은 트랜잭션 안에서 수행되어야 한다.
         entityTransaction.begin();
 
-        try{
+        try {
 //            // 데이터 삽입
 //            Member member = new Member();
 //            member.setId(1L);
 //            member.setName("helloA");
 //            // 엔티티 매니저의 영속성 컨텍스트에 위에서 만든 member 객체가 저장된다
-//            // 이제 member 엔티티는 엔티티 매니저의 관리 대상이 되고, 영속성을 가졌다고 말할 수 있다다
+//            // 이제 member 엔티티는 엔티티 매니저의 관리 대상이 되고, 영속성을 가졌다고 말할 수 있다
 //            entityManager.persist(member);
 //            entityTransaction.commit();
 
@@ -45,18 +45,48 @@ public class JpaMain {
 //            findMember.setName("helloJPA");
 //            entityTransaction.commit();
 
-            // JPQL
-            // table을 대상으로 코드를 짜지 않는다. Member 객체를 대상으로 쿼리를 짠다. 여기서 Member는 객체이다
-            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
-                    .getResultList();
+//            // JPQL
+//            // table을 대상으로 코드를 짜지 않는다. Member 객체를 대상으로 쿼리를 짠다. 여기서 Member는 객체이다
+//            List<Member> result = entityManager.createQuery("select m from Member as m", Member.class)
+//                    .getResultList();
+//
+//            for(Member member : result){
+//                System.out.println("member.name = " + member.getName());
+//            }
+//
+//            entityTransaction.commit();
 
-            for(Member member : result){
-                System.out.println("member.name = " + member.getName());
-            }
+            // 비영속 상태
+//            Member member = new Member();
+//            member.setId(101L);
+//            member.setName("helloJpa");
+//
+//            // 영속 상태
+//            // 엔티티 메니저 안에 있는 엔티티 컨텍스트라는 곳에서 member가 관리된다
+//            // 사실은 이 때 db에 저장되는 것은 아니다
+//            System.out.println("===Before==");
+//            entityManager.persist(member);
+//            System.out.println("==After==");
+//
+//            Member findMember = entityManager.find(Member.class, 101L);
+//            System.out.println("findMember.getId() : " + findMember.getId());
+//            System.out.println("findMember.getName() : " + findMember.getName());
+//
+//            entityTransaction.commit();
 
+//            Member findMember1 = entityManager.find(Member.class, 101L);
+//            findMember1.setName("updateName");
+//
+//            entityTransaction.commit();
+
+            Member member = new Member(200L, "member200");
+            entityManager.persist(member);
+
+            entityManager.flush();
+            System.out.println("-==============");
             entityTransaction.commit();
 
-        } catch (Exception e){
+        } catch (Exception e) {
             entityTransaction.rollback();
         } finally {
             entityManager.close();
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -12,6 +12,11 @@ public class Member {
     private Long id;
     private String name;
 
+    public Member(long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
