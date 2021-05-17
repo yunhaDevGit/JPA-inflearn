ba0c7adf8e2ccae63e53914b3303a047a3486f0c
yunhaDev <yunhaDev98@gmail.com>
2021-05-17 11:27:43 +0900

    feat: basic mapping in jpa

---
+++ jpastore/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class OrderItem {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @Column(name = "order_id")
+    private Long orderId;
+    @Column(name = "item_id")
+    private Long itemId;
+
+    private int orderPrice;
+    private int count;
+}
